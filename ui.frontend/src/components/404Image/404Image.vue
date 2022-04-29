78a5cc302dffce80b51c5df1d1ce1ef0cbe79ba4
Pedro Palmeira <pedro.palmeira@compasso.com.br>
2022-04-29 15:42:35 -0300

    ✨ Image Component for 404 page

---
+++ ui.frontend/src/components/404Image/404Image.vue
@@ -0,0 +1,15 @@
+<template>
+  <img :src="imagePath" />
+</template>
+<script>
+export default {
+  props: {
+    imagePath: { type: String, default: '' }
+  }
+}
+</script>
+<style lang="scss" scoped>
+img {
+  //
+}
+</style>
