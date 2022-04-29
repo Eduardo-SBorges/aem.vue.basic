79c8933f7810e257b11c4f8a1f98af852194ab5d
Pedro Palmeira <pedro.palmeira@compasso.com.br>
2022-04-29 15:39:21 -0300

    ✨ Text Component for 404 page

---
+++ ui.frontend/src/components/404Text/404Text.vue
@@ -0,0 +1,20 @@
+<template>
+  <h4>{{ text }}</h4>
+</template>
+<script>
+export default {
+  props: {
+    text: { type: String, default: 'Edit me!' }
+  }
+}
+</script>
+<style lang="scss" scoped>
+h4 {
+  font-family: 'Space Mono', monospace;
+  font-size: 24px;
+  font-weight: 400;
+  line-height: 36px;
+  letter-spacing: -0.035em;
+  color: #4f4f4f;
+}
+</style>
