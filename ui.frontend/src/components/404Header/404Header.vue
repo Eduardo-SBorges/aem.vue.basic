d6e4bda05acaf41838a7e13c78de5f02afa3c34d
Pedro Palmeira <pedro.palmeira@compasso.com.br>
2022-04-29 15:39:37 -0300

    ✨ Header Component for 404 page

---
+++ ui.frontend/src/components/404Header/404Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>{{ text }}</h2>
+</template>
+<script>
+export default {
+  props: {
+    text: { type: String, default: 'Edit me!' }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Inconsolata:wght@700&family=Montserrat:wght@500;600;700&family=Space+Mono:wght@400;700&display=swap');
+
+h2 {
+  font-family: 'Inconsolata', monospace;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 25px;
+  letter-spacing: -0.08em;
+  text-transform: uppercase;
+  color: #333;
+}
+</style>
