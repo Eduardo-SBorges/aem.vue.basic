30c29eb8c778d74bc929497bc7869038ea6cb4f8
Pedro Palmeira <pedro.palmeira@compasso.com.br>
2022-04-29 15:39:09 -0300

    ✨ Footer Component for 404 page

---
+++ ui.frontend/src/components/404Footer/404Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <h6>
+    created by <a :href="userurl">{{ username }}</a> - devChallenges.io
+  </h6>
+</template>
+<script>
+export default {
+  props: {
+    username: { type: String, default: 'Edit me!' },
+    userurl: { type: String, default: '#' }
+  }
+}
+</script>
+<style lang="scss" scoped>
+h6 {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 17px;
+  text-align: center;
+  color: #bdbdbd;
+
+  a {
+    font-weight: 700;
+    color: unset;
+  }
+}
+</style>
