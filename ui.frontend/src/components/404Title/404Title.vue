28cd50557428bf78ca48a1f55fccaae20296f266
Pedro Palmeira <pedro.palmeira@compasso.com.br>
2022-04-29 15:38:51 -0300

    ✨ Title Component for 404 page

---
+++ ui.frontend/src/components/404Title/404Title.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+<script>
+export default {
+  props: {
+    title: { type: String, default: 'Edit me!' }
+  }
+}
+</script>
+<style lang="scss" scoped>
+h1 {
+  font-family: 'Space Mono', monospace;
+  font-size: 64px;
+  font-weight: 700;
+  line-height: 95px;
+  letter-spacing: -0.035em;
+  color: #333;
+}
+</style>
