c53b1e82a597f4538ad6511a2b566f02aff6845a
Pedro Palmeira <pedro.palmeira@compasso.com.br>
2022-04-29 16:15:29 -0300

    ✨ Button Component for 404 page

---
+++ ui.frontend/src/components/404Button/404Button.vue
@@ -0,0 +1,40 @@
+<template>
+  <button class="btn" @click="goToHome">{{ text }}</button>
+</template>
+<script>
+export default {
+  props: {
+    text: { type: String, default: 'Edit me!' }
+  },
+  methods: {
+    goToHome () {
+      console.info('TODO: Redirect to home')
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+button.btn {
+  font-family: 'Space Mono', monospace;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 21px;
+  letter-spacing: -0.035em;
+  text-transform: uppercase;
+  border: none;
+  cursor: pointer;
+  padding: 24px 43px;
+  color: #fff;
+  background-color: #333;
+  transition: background-color 0.2s ease-in, box-shadow 0.1s ease-in-out;
+
+  &:hover {
+    background-color: #777;
+    transition: background-color 0.1s ease-in;
+  }
+
+  &:active {
+    box-shadow: inset 0 0 20px -3px #0008;
+  }
+}
+</style>
